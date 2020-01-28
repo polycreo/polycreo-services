f358ed63565b63b4fc32a47cdc79a2f974806c37
Daisuke Miyamoto <dai.0304@gmail.com>
2020-01-28 16:20:16 +0900

    avoid dependency on chunks (presentation)

--- src/main/kotlin/ChunkableService.kt
+++ src/main/kotlin/ChunkableService.kt
@@ -2,20 +2,13 @@ package org.polycreo.services
 
 import java.io.Serializable
 import org.polycreo.chunkrequests.Chunkable
-import org.polycreo.chunks.Chunk
-import org.polycreo.chunks.ChunkFactory
 import org.polycreo.repositories.ChunkableRepository
 
 interface ChunkableService<E, ID>
     where ID : Serializable,
           ID : Comparable<ID> {
 
-    val chunkFactory: ChunkFactory
-
     val repository: ChunkableRepository<E, ID>
 
-    fun findAll(chunkable: Chunkable): Chunk<E> {
-        val found = repository.findAll(chunkable)
-        return chunkFactory.createChunk(found, chunkable)
-    }
+    fun findAll(chunkable: Chunkable): List<E> = repository.findAll(chunkable)
 }
